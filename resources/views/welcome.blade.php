d08e684691d48f88ebd7a4fa1371bfc2b4146adf
Benoît de Biolley <benoit@lwt.be>
2024-06-08 20:41:15 +0200

    :bug: Fix scss styling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
 
         <!-- Styles -->
         @viteReactRefresh
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
     <body>
         <div id="example"></div>
